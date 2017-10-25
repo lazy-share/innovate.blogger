5ae8feeaa9643f8533f6207b2a295c6b09626c17
lzy <1059446341@qq.com>
2017-10-25 22:52:33 +0800

    修改配置文件

--- web/src/main-aot.ts
+++
@@ -1,4 +0,0 @@
-import {AppModuleNgFactory} from "../aot/src/app/app.module.ngfactory";
-import {platformBrowser} from "@angular/platform-browser";
-
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);

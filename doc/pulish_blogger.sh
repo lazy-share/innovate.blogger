672bb812ddb9368f2fbd7ed5882f800725d6399c
lzy <1059446341@qq.com>
2017-10-29 12:32:15 +0800

    更新自动化脚本

--- doc/pulish_blogger.sh
+++ doc/pulish_blogger.sh
@@ -1,113 +1,113 @@
 #! /bin/bash
+#Author: laizhiyuan
+#Created: 2017-10-24
+#Desc: build blogger publish script
 
 #init var
 date=`date +%Y%m%d-%H%M%S`
-back_images_dir=/usr/local/back/images
-back_web_node_modules_dir=/usr/local/back/node_modules/web
-back_server_node_modules_dir=/usr/local/back/node_modules/server
-back_images_zip=${back_images_dir}/public-${date}.tar.gz
-back_web_node_modules_zip=${back_web_node_modules_dir}/node_modules-${date}.tar.gz
-back_server_node_modules_zip=${back_server_node_modules_dir}/node_modules-${date}.tar.gz
-build_dir=/usr/local/build
+back_root_dir=/usr/local/back
+back_web_root_dir=${back_root_dir}/web
+back_server_root_dir=${back_root_dir}/server
+back_server_pre_publish_zip=server-${date}.tar.gz
+back_server_pre_publish_dir=${back_server_root_dir}/publish
+back_web_pre_publish_zip=web-${date}.tar.gz
+back_web_pre_publish_dir=${back_web_root_dir}/publish
+
+build_root_dir=/usr/local/build
+clone_project_dir=${build_root_dir}/innovate.blogger
 branch=develop
-project_name=innovate.blogger
-web_module=${build_dir}/${project_name}/web
-server_module=${build_dir}/${project_name}/server
-server_module_envfile=${server_module}/conf/environments.js
-server_module_pm2_name=blogger-server
-publish_front_sources_dir=/usr/local/publish/
-ng_build_dist_name=front
+build_web_sources_dir=${clone_project_dir}/web
+build_server_sources_dir=${clone_project_dir}/server
+server_module_envfile=${build_server_sources_dir}/conf/environments.js
+publish_server_name=blogger-server
+build_web_name=web
+
+publish_root_dir=/usr/local/publish
+upload_video_dir=${publish_root_dir}/media/video
+upload_image_header_dir=${publish_root_dir}/media/images/header
+upload_image_article_dir=${publish_root_dir}/media/images/article
+upload_image_photo_dir=${publish_root_dir}/media/images/photo
+init_header_file=${build_server_sources_dir}/public/web/images/header/initHead.jpg
 git_address=https://github.com/lzy369/innovate.blogger
 
+test ! -d ${upload_video_dir} && mkdir -p ${upload_video_dir}
+test ! -d ${upload_image_header_dir} && mkdir -p ${upload_image_header_dir}
+test ! -d ${upload_image_article_dir} && mkdir -p ${upload_image_article_dir}
+test ! -d ${upload_image_photo_dir} && mkdir -p ${upload_image_photo_dir}
+
 #make build dir
-if [ ! -d "${build_dir}" ];then
-	echo "=================>${build_dir} dir not exists, created it now"
-	mkdir -p ${build_dir}
+if [ ! -d "${build_root_dir}" ];then
+	echo "=================>${build_root_dir} dir not exists, created it now"
+	mkdir -p ${build_root_dir}
 else
-	echo "======================>${build_dir} exists"
+	echo "======================>${build_root_dir} exists"
 
 fi
-sleep 3s
+sleep 2s
 
-#make bakc images dir
-if [ ! -d "${back_images_dir}" ];then
-	echo "===============> ${back_images_dir} dir not exists, created it now"
-	mkdir -p ${back_images_dir}
+#make bakc root dir
+if [ ! -d "${back_root_dir}" ];then
+	echo "===============> ${back_root_dir} dir not exists, created it now"
+	mkdir -p ${back_root_dir}
 else
-	echo "===================> ${back_images_dir} dir exists"
+	echo "===================> ${back_root_dir} dir exists"
 fi
-sleep 3s
+sleep 2s
 
-#make bakc web node_modules dir
-if [ ! -d "${back_web_node_modules_dir}" ];then
-        echo "===============> ${back_web_node_modules_dir} dir not exists, created it now"
-        mkdir -p ${back_web_node_modules_dir}
+#make bakc web publish dir
+if [ ! -d "${back_web_pre_publish_dir}" ];then
+        echo "===============> ${back_web_pre_publish_dir} dir not exists, created it now"
+        mkdir -p ${back_web_pre_publish_dir}
 else
-        echo "===================> ${back_web_node_modules_dir} dir exists"
+        echo "===================> ${back_web_pre_publish_dir} dir exists"
 fi
-sleep 5s
+sleep 2s
 
-#make bakc server node_modules dir
-if [ ! -d "${back_server_node_modules_dir}" ];then
-        echo "===============> ${back_server_node_modules_dir} dir not exists, created it now"
-        mkdir -p ${back_server_node_modules_dir}
+#make bakc server publish dir
+if [ ! -d "${back_server_pre_publish_dir}" ];then
+        echo "===============> ${back_server_pre_publish_dir} dir not exists, created it now"
+        mkdir -p ${back_server_pre_publish_dir}
 else
-        echo "===================> ${back_server_node_modules_dir} dir exists"
+        echo "===================> ${back_server_pre_publish_dir} dir exists"
 fi
-sleep 3s
-
-#make publish dir dir
-if [ ! -d "${publish_front_sources_dir}" ];then
-        echo "===============> ${publish_front_sources_dir} dir not exists, created it now"
-        mkdir -p ${publish_front_sources_dir}
-else
-        echo "===================> ${publish_front_sources_dir} dir exists"
-fi
-sleep 3s
-
-#back images web and server node_modules after clear project
-if [ -d "${build_dir}/${project_name}" ];then
-	echo "=================> ${build_dir}/${project_name} exists, begin back images node_modules after delete ${build_dir}/${project_name}";
-	tar -zcvf ${back_images_zip} ${server_module}/public
-	tar -zcvf ${back_server_node_modules_zip} ${server_module}/node_modules
-	tar -zcvf ${back_web_node_modules_zip} ${web_module}/node_modules
-	rm -rf ${build_dir}/${project_name}
-
-fi;
-sleep 5s
+sleep 2s
 
 #clone project
-cd ${build_dir} && echo "============> current dir `pwd`"
+cd ${build_root_dir} && echo "============> current dir `pwd`"
+test -d ${clone_project_dir} && rm -rf ${clone_project_dir}
 git clone -b ${branch} ${git_address}
 test $? != 0 && echo "git clone faild" && exit 1
 sleep 3s
 
-#recovery images
-rm -rf ${server_module}/public
-mkdir -p ${server_module}/public
-mkdir -p ${server_module}/node_modules
-mkdir -p ${web_module}/node_modules
-tar -zxvf ${back_images_zip} -C /
-tar -zxvf ${back_web_node_modules_zip} -C /
-tar -zxvf ${back_server_node_modules_zip} -C /
-sleep 5s
-
-#install server
+#update server module env
 sed -i "s/dev/prod/g" ${server_module_envfile}
-cd ${server_module} && npm install
-pid=pm2 list | grep "${server_module_pm2_name}" | awk '{print $12}'
-if [ $pid != '' ]; then
-	kill -9 $pid
-fi
-pm2 start ./bin/www --name ${server_module_pm2_name}
 
-#install web
-cd ${web_module} && npm install
-cp -R ../temp/uploadimage ./node_modules/tinymce/plugins
+#install
+cd ${build_server_sources_dir} && npm install
+cd ${build_web_sources_dir} && npm install 
+
+#copy required file
+cp -R ${clone_project_dir}/temp/uploadimage ${build_web_sources_dir}/node_modules/tinymce/plugins
+cp  ${init_header_file} ${upload_image_header_dir}/
+
+#build web pages
 npm run build
-rm -rf ${publish_front_sources_dir}/*
-cp -R ${web_module}/${ng_build_dist_name} ${publish_front_sources_dir}/
- 
 
+#back pre publish version
+test -d ${publish_root_dir}/server && tar -zcvf ${back_server_pre_publish_dir}/${back_server_pre_publish_zip} ${publish_root_dir}/server && rm -rf ${publish_root_dir}/server
+test -d ${publish_root_dir}/web && tar -zcvf ${back_web_pre_publish_dir}/${back_web_pre_publish_zip} ${publish_root_dir}/web && rm -rf ${publish_root_dir}/web
 
+#copy current resources to publish
+cp -R ${build_server_sources_dir} ${publish_root_dir}/
+cp -R ${build_web_sources_dir}/web ${publish_root_dir}/
+
+#restart server
+pid=`pm2 list | grep "${publish_server_name}" | awk '{print $2}'`
+echo "================= pid: ${pid} " 
+if [ $pid != '' ]; then
+	pm2 restart ${publish_server_name}
+else
+	pm2 start ${publish_root_dir}/server/bin/www --name "${publish_server_name}"
+
+fi
 

081c64b8a8a4aec8a0b4951de29f64dfee69efab
laizhiyuan <13423803799@163.com>
2017-11-02 14:44:57 +0800

    修改配置文件

---
+++ web/src/main-aot.ts
@@ -0,0 +1,4 @@
+import { AppModuleNgFactory } from "../aot/src/app/app.module.ngfactory";
+import { platformBrowser } from "@angular/platform-browser";
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
+//# sourceMappingURL=main-aot.js.map
